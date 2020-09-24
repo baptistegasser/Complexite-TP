4b260b8a0b796726d9f0cdf9c908778cbbc56e5f
lucco <luc.bobo@etu.univ-amu.fr>
2020-09-24 17:32:09 +0200

    OUI

--- src/main/java/Exercice1/RecursivFibonacci.java
+++ src/main/java/Exercice1/RecursivFibonacci.java
@@ -3,14 +3,14 @@ import java.math.BigInteger;
 
 public class RecursivFibonacci {
 
-    public static BigInteger fibo (int n, BigInteger F1 , BigInteger F0) {
+    public static BigInteger fibo (int maxValue, BigInteger F1 , BigInteger F0) {
         // Retourne la valeur de F0 et F1
-        if (n == 0) return F0;
-        else if (n == 1) return F1;
+        if (maxValue == 0) return F0;
+        else if (maxValue == 1) return F1;
         else {
-            n = n - 1;
+            maxValue = maxValue - 1;
         }
-        return fibo(n, (F1.add(F0)), F1);
+        return fibo(maxValue, (F1.add(F0)), F1);
     }
 
     public static void main (String[] args) {
