1b51c24a113fc11ed7109e9ab2e9f8aab3548be4
Baptiste <baptiste.arnoulet@etu.univ-amu.fr>
2020-10-30 15:13:05 +0100

    Clean de code et optimisation

--- src/main/java/exercice2/App.java
+++ src/main/java/exercice2/App.java
@@ -12,49 +12,52 @@ public class App {
         //graph.show();
 
         //Question 1
-        int[] zoneAChercher = new int[]{2,4};
+        int[] zoneAChercher = new int[]{1,5};
 
         if (graph.zoneVide(zoneAChercher)) {
             System.out.println(Arrays.toString(zoneAChercher) + " : Zone vide");
         } else System.out.println(Arrays.toString(zoneAChercher) + " : Zone non-vide");
 
-        long start = System.currentTimeMillis();
-
         //Question 2
+        // Valeur pour les test de temps d'exécution
+        int nbExe = 500000;
+
         System.out.println("Une zone vide maximal : "+ graph.zoneVideMaximal());
 
-        for (int i = 0; i<5000;i++) {
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i<nbExe;i++) {
             graph.zoneVideMaximal();
         }
 
         long end = System.currentTimeMillis();
 
-        System.out.println("Time for Q2 : "+(end-start)+"ms");
+        System.out.println("Temps pour "+nbExe+" itération de Q2 : "+(end-start)+"ms");
 
         //Question 3
 
-        long start2 = System.currentTimeMillis();
-
         System.out.println("Une zone vide maximum (Methode complète) : "+ graph.zoneVideMaximumCom());
 
-        for (int i = 0; i<5000;i++) {
+        long start2 = System.currentTimeMillis();
+
+        for (int i = 0; i<nbExe;i++) {
             graph.zoneVideMaximumCom();
         }
 
         long end2 = System.currentTimeMillis();
-        System.out.println("Time for Q3 : "+(end2-start2)+"ms");
+        System.out.println("Temps pour "+nbExe+" itération de Q3 : "+(end2-start2)+"ms");
 
         //Question 4
 
-        long start3 = System.currentTimeMillis();
-
         System.out.println("Une zone vide maximum (Methode incomplète) : "+ graph.zoneVideMaximumInc());
 
-        for (int i = 0; i<5000;i++) {
+        long start3 = System.currentTimeMillis();
+
+        for (int i = 0; i<nbExe;i++) {
             graph.zoneVideMaximumInc();
         }
 
         long end3 = System.currentTimeMillis();
-        System.out.println("Time for Q4 : "+(end3-start3)+"ms");
+        System.out.println("Temps pour "+nbExe+" itération de Q4 : "+(end3-start3)+"ms");
     }
 }
--- src/main/java/exercice2/Graph.java
+++ src/main/java/exercice2/Graph.java
@@ -2,7 +2,7 @@ package exercice2;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.zip.ZipEntry;
+import java.util.stream.Stream;
 
 public class Graph {
     int[][] matrice;
@@ -12,14 +12,32 @@ public class Graph {
     }
 
     /**
+     * Vérifie si la la liste de sommet passée en paramètre est une zone vide
+     * @param listSommet liste de sommet à vérifier
+     * @return vrai si listSommet est une zone vide, faux sinon
+     */
+    public boolean zoneVide(int[] listSommet) {
+        if (listSommet.length == 0) return false; //Si la liste est vide
+        if (listSommet.length == 1) { // Si on cherche sur un seul sommet
+            return !haveEdge(listSommet[0] - 1, listSommet[0] - 1); // On test avec lui même
+        }
+
+        for (int sommet = 0; sommet<listSommet.length; sommet++) {
+            for (int sommetACompare = sommet; sommetACompare < listSommet.length; sommetACompare++) {
+                if (haveEdge(listSommet[sommet] - 1, listSommet[sommetACompare] - 1)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
      * Retourne une liste de sommet formant une zone vide maximal
      * @return
      */
     public ArrayList<Integer> zoneVideMaximal() {
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i<matrice.length;i++) list.add(i);
-
-        return parcours(list);
+        return parcours(null);
     }
 
     /**
@@ -27,9 +45,51 @@ public class Graph {
      * @return
      */
     public ArrayList<Integer> zoneVideMaximumCom() {
-        ArrayList<Integer> list = triSommetByNbedge();
+        ArrayList<Integer> list = (ArrayList<Integer>) triSommetByNbedge();
         return parcours(list);
     }
+
+    /**
+     * Parcours la liste de sommets du graph et retourne une zone vide
+     * @param list liste à parcourir
+     * @return une liste de sommet formant une zone vide
+     */
+    public ArrayList<Integer> parcours(ArrayList<Integer> list) {
+        //Init list de visited
+        ArrayList<Integer> returnList = new ArrayList<>();
+        ArrayList<Boolean> listVisited = new ArrayList<>();
+        //Si la list n'est pas initialisé, on la rempli avec les sommets dans l'ordre croissant
+        if (list==null) {
+            list = new ArrayList<>();
+            for (int i = 0; i<matrice.length;i++){
+                listVisited.add(true);
+                list.add(i);
+            }
+        } else {
+            for (int i = 0; i < list.size(); i++) {
+                listVisited.add(true);
+            }
+        }
+
+        //Parcours
+        for (Integer sommet : list) {
+            //Si il n'est pas marqué
+            if (listVisited.get(sommet)) {
+                //On parcours tous ces voisins pour les marquer
+                ArrayList<Integer> listVoisin = getVoisin(sommet);
+                for (Integer voisin : listVoisin) {
+                    listVisited.set(voisin, false);
+                }
+            }
+            //Si il n'a pas été marqué (permet des gérer les boucles)
+            if (listVisited.get(sommet)) {
+                returnList.add(sommet+1);
+            }
+        }
+
+        return returnList;
+    }
+
 
     /**
      * Retourne une liste de sommet formant une zone vide maximum (methode incomplète)
@@ -41,107 +101,63 @@ public class Graph {
         ArrayList<Boolean> listVisitedParcours = new ArrayList<>();
         ArrayList<Integer> list = new ArrayList<>();
 
+        //Init
         for (int i = 0; i<matrice.length;i++){
             list.add(i);
             listVisitedParcours.add(true);
             listVisited.add(false);
         }
 
-        for (int sommet = 0; sommet<list.size(); sommet++) {
-            ArrayList<Integer> listVoisin = getVoisin(sommet);
-            for (int voisin = 0; voisin<listVoisin.size(); voisin++) {
-                if (listVisited.get(listVoisin.get(voisin))) {
-                    listVisitedParcours.set(sommet, false);
-                    break;
-                }
-                else if (listVisitedParcours.get(listVoisin.get(voisin))) {
-                    if (getNbEdge(listVoisin.get(voisin)) < getNbEdge(sommet)) {
+        for (Integer sommet : list) {
+            //On parcours les voisins
+            for (Integer voisin : list) {
+                if (matrice[sommet][voisin] == 1) {
+                    //si il a déjà été pris
+                    if (listVisited.get(voisin)) {
                         listVisitedParcours.set(sommet, false);
                         break;
+                    } // si on la déjà visité mais pas pris
+                    else if (listVisitedParcours.get(voisin)) {
+                        //Si mon voisin à moins de voisin que moi
+                        if (getNbEdge(voisin) < getNbEdge(sommet)) {
+                            listVisitedParcours.set(sommet, false);
+                            break;
+                        }
                     }
                 }
             }
+
             if (listVisitedParcours.get(sommet)) {
                 listVisited.set(sommet, true);
                 listVisitedParcours.set(sommet, false);
             }
-        }
 
-        for (int i = 0; i<listVisited.size(); i++) {
-            if (listVisited.get(i)) returnList.add(i+1);
-        }
-
-        return returnList;
-    }
-
-    public ArrayList<Integer> parcours(ArrayList<Integer> list) {
-        //Init list de visited
-        ArrayList<Boolean> listVisited = new ArrayList<>();
-        for (int i = 0; i<list.size();i++){
-            listVisited.add(true);
-        }
-
-        for (Integer i : list) {
-            //Si il n'est pas viré
-            if (listVisited.get(i)) {
-                //On parcours tous ces voisins pour les virer
-                ArrayList<Integer> listVoisin = getVoisin(i);
-                for (Integer voisin : listVoisin) {
-                    listVisited.set(voisin, false);
-                }
+            //On rempli la liste de retour
+            if (listVisited.get(sommet)) {
+                returnList.add(sommet+1);
             }
         }
 
-        //On rempli la liste de sortie
-        ArrayList<Integer> returnList = new ArrayList<>();
-        for (int i = 0; i<listVisited.size(); i++) {
-            if (listVisited.get(i)) returnList.add(i+1);
-        }
-
         return returnList;
     }
-
 
     /**
      *  Retourne la liste de sommet triè en fonction du nombre de voisin décroissant
      */
-    public ArrayList<Integer> triSommetByNbedge() {
+    public List<Integer> triSommetByNbedge() {
         Map<Integer, Integer> m = new HashMap<>();
         for (int i = 0; i<matrice.length;i++) {
             m.put(i,getNbEdge(i));
         }
-        Map<Integer, Integer> result = m.entrySet()
+
+        Stream<Integer> resultStream = m.entrySet()
                 .stream()
                 .sorted(Map.Entry.comparingByValue())
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+                .map(Map.Entry::getKey);
 
-        return new ArrayList<>(result.keySet());
-    }
+        List<Integer> result = resultStream.collect(Collectors.toList());
 
-
-    /**
-     * Vérifie si la la liste de sommet passée en paramètre est une zone vide
-     * @param listSommet
-     * @return
-     */
-    public boolean zoneVide(int[] listSommet) {
-        if (listSommet.length == 0) return false; //Si la liste est vide
-
-        for (int sommmet = 0; sommmet<listSommet.length; sommmet++) {
-            if (listSommet.length == 1) { // Si on cherche sur un seul sommet
-                return !haveEdge(listSommet[sommmet] - 1, listSommet[sommmet] - 1); // On test avec lui même
-            } else { // Si on cherche entre plusieurs sommets
-                for (int sommetACompare = sommmet+1; sommetACompare < listSommet.length; sommetACompare++) {
-                    if (haveEdge(listSommet[sommmet] - 1, listSommet[sommetACompare] - 1)) {
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
+        return result;
     }
 
     /**
