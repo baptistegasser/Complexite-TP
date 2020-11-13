ca8eb96c6e19280e76a970f01ff2e44524962de3
lucco <luc.bobo@etu.univ-amu.fr>
2020-11-13 20:59:05 +0100

    JavaDoc FNCSolver

--- src/main/java/TP2/Exercice1/FNCSolver.java
+++ src/main/java/TP2/Exercice1/FNCSolver.java
@@ -2,14 +2,25 @@ package TP2.Exercice1;
 
 import java.util.ArrayList;
 
+/**
+ * Cette classe permet de savoir si une formume FNC est satisfiable
+ */
 public class FNCSolver {
 
     int nbTermes;
-    ArrayList<ArrayList<Pair<Integer, Boolean>>> fncList;
-    ArrayList<Boolean> valueOfTerms = new ArrayList<>();
 
     /**
-     * Méthode qui permet de dire si la FNC est SAT
+     * Liste des clauses, qui contiennent des Pair<numTerme,valeur>
+     */
+    ArrayList<ArrayList<Pair<Integer, Boolean>>> fncList;
+
+    /**
+     * Valeur des termes passé en paramètre dans le .txt
+     */
+    ArrayList<Boolean> valueOfTerms;
+
+    /**
+     * Méthode génerale permettant de traiter une formule FNC : lecture de fichier et résolution
      */
     public void solve () {
         FNCReader fileReader = new FNCReader("C:\\Users\\lucco\\IdeaProjects\\Complexite-TP\\src\\main\\java\\TP2\\Exercice1\\data.cnf");
@@ -21,18 +32,19 @@ public class FNCSolver {
     }
 
     /**
-     * Vérifie si la FNC est SAT avec les listes données.
+     * Vérifie si la FNC est SAT avec les listes de la classe.
      */
     public boolean isSat () {
         // Pour chaque clause
         for (ArrayList<Pair<Integer, Boolean>> currentList: fncList) {
             // On vérifie si au moins une valeur est à vrai
             boolean isClauseGood = false;
+            // Pour chaque termes de la clause
             for (Pair<Integer, Boolean> currentPair: currentList) {
                 int xindice = currentPair.getKey();
-                //car list de taille termes - 1
+                //Car liste de taille termes - 1
                 boolean termValue = valueOfTerms.get(xindice - 1);
-                // Dans les deux cas suivant, une clause est à vrai : la clause est validée
+                // Dans les deux cas suivant, un terme est à vrai : la clause est validée
                 if(currentPair.getValue() && termValue) {
                     isClauseGood = true;
                     break;
@@ -42,9 +54,10 @@ public class FNCSolver {
                     break;
                 }
             }
-            // La clause n'a pas été passé à vrai : return false
+            // La clause n'a pas été passé à vrai car tout les termes sont faux : la FNC est fausse
             if(!isClauseGood) return false;
         }
+        // On arrive ici si toutes les clauses ont été vraies une fois
         return true;
     }
 
