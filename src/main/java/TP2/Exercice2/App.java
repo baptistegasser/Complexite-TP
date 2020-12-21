31bf0495a9273057867c46e9a746617f4479ff4b
Baptiste <baptiste.arnoulet@etu.univ-amu.fr>
2020-12-21 20:47:24 +0100

    Je sais pas.sat

--- src/main/java/TP2/Exercice2/App.java
+++ src/main/java/TP2/Exercice2/App.java
@@ -1,13 +1,25 @@
 package TP2.Exercice2;
 
+import TP2.minisat.MiniSat;
+
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class App {
     public static void main(String[] args) throws IOException {
         Graph graph = new Graph(MatriceTest.matrice2);
 
-        Resolver resolver = new Resolver();
+        GraphToSAT resolver = new GraphToSAT();
 
-        resolver.reduction(graph);
+        //resolver.resolution(graph);
+
+        String testSource = "src/main/resources/test.sat";
+        File file = new File(testSource);
+
+        boolean res = MiniSat.run(file);
+
+        if (res) System.out.println("Test Sat");
+        else System.out.println("test Insat");
     }
 }
--- src/main/java/TP2/Exercice2/GraphToSAT.java
+++ src/main/java/TP2/Exercice2/GraphToSAT.java
@@ -1,6 +1,10 @@
 package TP2.Exercice2;
 
+import TP2.minisat.MiniSat;
+import TP2.minisat.Util;
+
 import javax.swing.*;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,10 +12,22 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Resolver {
-    Path fichier = Paths.get("src/main/java/TP2/Exercice2/formule.txt");
+public class GraphToSAT {
+    String source = "src/main/java/TP2/Exercice2/formule.txt";
+    Path fichier = Paths.get(source);
 
-    public Resolver() throws IOException {
+    public GraphToSAT() throws IOException {
+    }
+
+    public void resolution(Graph graph) throws IOException {
+        this.reduction(graph);
+
+        File file = new File(source);
+
+        boolean result = MiniSat.run(file);
+
+        if (result) System.out.println("Satisfiable");
+        else System.out.println("Insatisfiable");
     }
 
     public void reduction(Graph graph) throws IOException {
@@ -25,7 +41,6 @@ public class Resolver {
             for (int j = i+1; j<graph.getMatrice().length;j++) {
                 if (graph.getMatrice()[i][j] == 1) {
                     lignes.add(i +" "+ j);
-
                 }
             }
         }
