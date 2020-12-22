40a964c04daee673caaf12166a756da890857b1e
Baptiste <baptiste.arnoulet@etu.univ-amu.fr>
2020-12-22 19:19:12 +0100

    Add doc in exo 2

--- src/main/java/TP2/Exercice2/GraphToSAT.java
+++ src/main/java/TP2/Exercice2/GraphToSAT.java
@@ -19,6 +19,13 @@ public class GraphToSAT {
     public GraphToSAT() throws IOException {
     }
 
+    /**
+     * Resolution by MiniSat the 3-Sat problem
+     *
+     * @param graph the graph
+     * @param k the desired empty zone size
+     * @throws IOException
+     */
     public void resolution(Graph graph, int k) throws IOException {
         this.reduction(graph,k);
 
@@ -30,63 +37,93 @@ public class GraphToSAT {
         else System.out.println("Pour k = "+k+", la solution est insatisfiable \n");
     }
 
+    /**
+     * Reduction a graph to 3-Sat problem
+     *
+     * @param graph the graph
+     * @param k the desired empty zone size
+     * @throws IOException
+     */
     public void reduction(Graph graph, int k) throws IOException {
-        System.out.println("Reduction");
+        int nbVertices = graph.getMatrice().length;
+        int clauseSize = nbVertices - (k-1);
 
-        int nbSommet = graph.getMatrice().length;
+        ArrayList<String> lines = new ArrayList<>();
 
-        ArrayList<String> lignes = new ArrayList<>();
+        int nbClause = (int) (graph.getNbEdge() + binomi(nbVertices, clauseSize));
 
-        //Première lignes
-        lignes.add("p cnf " + nbSommet +" "+ graph.getNbEdge());
+        //First line
+        lines.add("p cnf " + nbVertices +" "+ nbClause);
 
 
-        // Ajout des cluases pour chaque arc
-        for (int i = 0; i<nbSommet;i++) {
-            for (int j = i+1; j<nbSommet;j++) {
+        // Add clause for each edge
+        for (int i = 0; i<nbVertices;i++) {
+            for (int j = i+1; j<nbVertices;j++) {
                 if (graph.getMatrice()[i][j] == 1) {
-                    lignes.add("-"+(i+1) +" -"+ (j+1) + " 0");
+                    lines.add("-"+(i+1) +" -"+ (j+1) + " 0");
                 }
             }
         }
 
 
-        int tailleClause = nbSommet - (k-1);
-
-        // Ajout des clauses en fonction de k
+        // Add clause in function k
         StringBuilder currentBuilder = new StringBuilder();
-        for (int i = 0; i < nbSommet ; i++) {
+        for (int i = 0; i < nbVertices ; i++) {
             StringBuilder nextBuilder = new StringBuilder(currentBuilder);
 
-            //Ajout du sommet  actuel
+            // Add current vertice
             nextBuilder.append(i+1).append(" ");
-            //Si la taille de la clause a été atteinte
-            if (tailleClause > 1)
-                recursion(i,tailleClause, nbSommet, nextBuilder, lignes);
+            // If clause size is reached
+            if (clauseSize > 1)
+                recursion(i,clauseSize, nbVertices, nextBuilder, lines);
             else {
                 nextBuilder.append("0");
-                lignes.add(nextBuilder.toString());
+                lines.add(nextBuilder.toString());
             }
         }
 
-        Files.write(fichier, lignes);
+        Files.write(fichier, lines);
     }
 
-    public void recursion(int x, int tailleClause, int nbSommet, StringBuilder currentBuilder, ArrayList<String> lignes) {
-        tailleClause -= 1;
+    /**
+     * Write clauses for take value in function of k
+     *
+     * @param x the current index
+     * @param clauseSize the current size of clause
+     * @param nbVertices the number of vertices
+     * @param currentBuilder The string builder
+     * @param lines The list of clause
+     */
+    public void recursion(int x, int clauseSize, int nbVertices, StringBuilder currentBuilder, ArrayList<String> lines) {
+        clauseSize -= 1;
 
-        for (int i = x+1; i < nbSommet ; i++) {
+        for (int i = x+1; i < nbVertices ; i++) {
             StringBuilder nextBuilder = new StringBuilder(currentBuilder);
 
-            //Ajout du sommet  actuel
+            // Add current vertice
             nextBuilder.append(i + 1).append(" ");
-            //Si la taille de la clause a été atteinte
-            if (tailleClause > 1)
-                recursion(i,tailleClause, nbSommet,nextBuilder, lignes);
+            // If clause size is reached
+            if (clauseSize > 1)
+                recursion(i,clauseSize, nbVertices,nextBuilder, lines);
             else {
                 nextBuilder.append("0");
-                lignes.add(nextBuilder.toString());
+                lines.add(nextBuilder.toString());
             }
         }
+    }
+
+    /**
+     * Take this function from https://stackoverflow.com/questions/36925730/java-calculating-binomial-coefficient
+     *
+     * Calculate the binomial coefficient between two variables
+     * @param n first variable
+     * @param k second variable
+     * @return the binomial coefficient
+     */
+    static long binomi(int n, int k) {
+        if ((n == k) || (k == 0))
+            return 1;
+        else
+            return binomi(n - 1, k) + binomi(n - 1, k - 1);
     }
 }
