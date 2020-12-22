b494c02eee3e665c0f84cd8f54bb84315d546015
Baptiste <baptiste.arnoulet@etu.univ-amu.fr>
2020-12-22 21:32:39 +0100

    Add de brute force et de doc et de calcul de temps

--- src/main/java/TP2/Exercice2/MatriceTest.java
+++ src/main/java/TP2/Exercice2/MatriceTest.java
@@ -4,9 +4,9 @@ public class MatriceTest {
     public static final int[][] matrice1 = new int[][] {
             {0,1,0,0,0},
             {1,0,0,1,0},
-            {0,0,0,0,0},
-            {0,1,0,0,0},
-            {0,0,0,0,0}};
+            {0,0,0,1,0},
+            {0,1,1,0,1},
+            {0,0,0,1,0}};
 
     public static final int[][] matrice2 = new int[][] {
             {0,1,0,1,0,0,1},
